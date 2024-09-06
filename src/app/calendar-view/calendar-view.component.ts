4fcd1d7abed11f8b9838800341f7d508c53553e4
Ahmed Ejaz <ahm.ejaz791@gmail.com>
2024-09-06 19:46:26 +0500

    need to implement drag and drop

--- src/app/calendar-view/calendar-view.component.ts
+++ src/app/calendar-view/calendar-view.component.ts
@@ -1,10 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-calendar-view',
@@ -18,17 +27,26 @@ import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform
     CommonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    CdkDropList,
+    CdkDrag,
+    FormsModule,
+    MatButtonModule
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CalendarViewComponent {
+  @ViewChild('datetxt') datetxtEl: ElementRef;
+  @ViewChild('dates') datesEl: ElementRef;
+  @ViewChild('month_year') monthYearEl: ElementRef;
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
   dateObj: any;
   dayName: any;
   month: any;
   year: any;
   date: any;
-  datetxtEl: any = document.querySelector("#datetxt");
+  SelectedDate: any = null;
   dmObj = {
     days: [
       "Sunday",
@@ -54,15 +72,15 @@ export class CalendarViewComponent {
       "December",
     ],
   };
-  datesEl: any = document.querySelectorAll("#dates");
-  btnEl: any = document.querySelectorAll("#calendar_headings");
-  monthYearEl: any = document.querySelectorAll("#month_year");
+  datesElement: any;
+  monthYearElement: any;
   firtDayOfMonth: any = null;
   lastDateofMonth: any = null;
   lastDayofMonth: any = null;
   lastDateofLastMonth: any = null;
+  getDateFromUser: any = [];
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
     // date object
@@ -71,55 +89,127 @@ export class CalendarViewComponent {
     this.month = this.dateObj.getMonth(); // month
     this.year = this.dateObj.getFullYear(); // year
     this.date = this.dateObj.getDate(); // dates;
-    // today date
-    if (this.datetxtEl) {
-      this.datetxtEl.innerHTML = `${this.dayName},${this.date}, ${this.dmObj.months[this.month]}, ${this.year}`;
+  }
+
+  ngAfterViewInit(): void {
+    this.datetxtEl.nativeElement.innerHTML = `${this.dayName},${this.date}, ${this.dmObj.months[this.month]}, ${this.year}`;
+    this.datesElement = this.datesEl.nativeElement;
+    this.monthYearElement = this.monthYearEl.nativeElement;
+    this.monthYearElement.innerHTML = `${this.dmObj.months[this.month]}, ${this.year}`;
+    this.displayCalendar();
+  }
+
+  prevMonth() {
+    this.month = this.month - 1
+    if (this.month < 0 || this.month > 11) {
+      this.date = new Date(this.year, this.month, new Date().getDate());
+      this.year = this.date.getFullYear();
+      this.month = this.date.getMonth();
+    } else {
+      this.date = new Date();
     }
     this.displayCalendar();
-    // previous and next month
-    this.btnEl.forEach((btns: any) => {
-      btns.addEventListener("click", () => {
-        this.month = btns.id === "prev" ? this.month - 1 : this.month + 1;
-        if (this.month < 0 || this.month > 11) {
-          this.date = new Date(this.year, this.month, new Date().getDate());
-          this.year = this.date.getFullYear();
-          this.month = this.date.getMonth();
-        } else {
-          this.date = new Date();
-        }
-        this.displayCalendar();
-      });
-    });
+  }
+
+  nextMonth() {
+    this.month = this.month + 1
+    if (this.month < 0 || this.month > 11) {
+      this.date = new Date(this.year, this.month, new Date().getDate());
+      this.year = this.date.getFullYear();
+      this.month = this.date.getMonth();
+    } else {
+      this.date = new Date();
+    }
+    this.displayCalendar();
   }
 
   displayCalendar() {
-    this.firtDayOfMonth = new Date(this.year, this.month, 1).getDay(); // first day of the month
-    this.lastDateofMonth = new Date(this.year, this.month + 1, 0)?.getDate(); // last date of the month
-    this.lastDayofMonth = new Date(this.year, this.month, this.lastDateofMonth)?.getDay(); // last day of month
+    this.firtDayOfMonth = new Date(this.year, this.month, 1).getDay();
+    this.lastDateofMonth = new Date(this.year, this.month + 1, 0)?.getDate();
+    this.lastDayofMonth = new Date(this.year, this.month, this.lastDateofMonth)?.getDay();
     this.lastDateofLastMonth = new Date(this.year, this.month, 0)?.getDate();
-    let days = "";
+    let days: any = "";
+    let iterator: any = [];
     // previous month last days
     for (let i = this.firtDayOfMonth; i > 0; i--) {
-      days += `<li class="dummy">${this.lastDateofLastMonth - i + 1}</li>`;
+      for (let j = 0; j < this.getDateFromUser.length; j++) {
+        if (this.getDateFromUser[j] === `${this.lastDateofLastMonth - i + 1}/${this.month - 1}/${this.year}`) {
+          days += `<li class="dummy ${this.lastDateofLastMonth - i + 1}/${this.month - 1}/${this.year}">${this.lastDateofLastMonth - i + 1} ${this.getDateFromUser[j] === `${this.lastDateofLastMonth - i + 1}/${this.month - 1}/${this.year}` ? this.getAppointmentTemplate(`${this.lastDateofLastMonth - i + 1}/${this.month - 1}/${this.year}`) : ''} </li>`;
+          iterator.push(i);
+        } 
+      }
+      if (this.getDateFromUser.length <= 0 || (!iterator.includes(i))) {
+        days += `<li class="dummy ${this.lastDateofLastMonth - i + 1}/${this.month - 1}/${this.year}">${this.lastDateofLastMonth - i + 1} </li>`;
+      }
     }
+    iterator = [];
     for (let i = 1; i <= this.lastDateofMonth; i++) {
       let checkToday =
         i === this.dateObj.getDate() &&
-          this.month === new Date().getMonth() &&
+          this.month + 1 === new Date().getMonth() + 1 &&
           this.year === new Date().getFullYear()
-          ? "active"
-          : "";
-      days += `<li class="${checkToday}">${i}</li>`;
+          ? `active ${i}/${this.month + 1}/${this.year}`
+          : `${i}/${this.month + 1}/${this.year}`;
+      for (let j = 0; j < this.getDateFromUser.length; j++) {
+        if (this.getDateFromUser[j] === `${i}/${this.month + 1}/${this.year}`) {
+          days += `<li class="${checkToday}">${i} ${this.getDateFromUser[j] === `${i}/${this.month + 1}/${this.year}` ? this.getAppointmentTemplate(`${i}/${this.month + 1}/${this.year}`) : ''}</li>`;
+          iterator.push(i);
+        }
+      }
+      if (this.getDateFromUser.length <= 0 || (!iterator.includes(i))) {
+        days += `<li class="${checkToday}">${i}</li>`;
+      }
     }
+    iterator = [];
     //next month first days
     for (let i = this.lastDayofMonth; i < 6; i++) {
-      days += `<li class="dummy">${i - this.lastDayofMonth + 1}</li>`;
+      for (let j = 0; j < this.getDateFromUser.length; j++) {
+        if (this.getDateFromUser[j] === `${i - this.lastDayofMonth + 1}/${this.month}/${this.year}`) {
+          days += `<li class="dummy ${i - this.lastDayofMonth + 1}/${this.month}/${this.year}">${i - this.lastDayofMonth + 1} ${this.getDateFromUser[j] === `${i - this.lastDayofMonth + 1}/${this.month}/${this.year}` ? this.getAppointmentTemplate(`${i - this.lastDayofMonth + 1}/${this.month}/${this.year}`) : ''}</li>`;
+          iterator.push(i);
+        }
+      }
+      if (this.getDateFromUser.length <= 0 || !iterator.includes(i)) {
+        days += `<li class="dummy ${i - this.lastDayofMonth + 1}/${this.month}/${this.year}">${i - this.lastDayofMonth + 1}</li>`;
+      }
     }
     // display all days inside the HTML file
-    if (this.datesEl) {
-      this.datesEl.innerHTML = days;
-    }
-    // display current month & year
-    this.monthYearEl.innerHTML = `${this.dmObj.months[this.month]}, ${this.year}`;
+    this.appendToHtml(days);
   };
+
+  appendToHtml(days: any) {
+    this.datesElement.innerHTML = days;
+    this.monthYearElement.innerHTML = `${this.dmObj.months[this.month]}, ${this.year}`;
+  }
+
+  getAppointmentTemplate(date: any) {
+    return `<div class='appointment ${date}' cdkDrag style='padding: 0.5rem;margin: 0.2rem;background-color: #000;border-radius: 5px;color: #fff;'>✅booked</div>`
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  onSubmit() {
+    if (this.SelectedDate) {
+      if (!(this.getDateFromUser.includes(`${this.SelectedDate.getDate()}/${this.SelectedDate.getMonth() + 1}/${this.SelectedDate.getFullYear()}`))) {
+        this.getDateFromUser = [
+          ...this.getDateFromUser,
+          `${this.SelectedDate.getDate()}/${this.SelectedDate.getMonth() + 1}/${this.SelectedDate.getFullYear()}`
+        ]
+      }
+      this.displayCalendar();
+    } else {
+      this.getDateFromUser = undefined;
+    }
+  }
 }
